5a755fb5fb2611482af428f23e7d08aeaa7bb4db
Vance Palacio <vanceism7@gmail.com>
2018-09-09 16:57:09 -0700

    Pulled activities data out of ScheduleView. Prepping for redux

--- src/UI/ActvitiyEntry.js
+++ src/UI/ActvitiyEntry.js
@@ -2,48 +2,82 @@ import React from 'react'
 import '../Styles/Components/ActivityEntry.css'
 
 class ActivityEntry extends React.Component {
-  /* Properties
-    this.props.Activity.id
-    this.props.Activity.StartTime
-    this.props.Activity.EndTime
-  */
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      id: this.props.Activity.id,
+      Title: this.props.Activity.Title,
+      StartTime: this.props.Activity.StartTime,
+      EndTime: this.props.Activity.EndTime,
+      Description: this.props.Activity.Description
+    }
+
+    this.TitleChanged = this.TitleChanged.bind(this)
+    this.StartTimeChanged = this.StartTimeChanged.bind(this)
+    this.EndTimeChanged = this.EndTimeChanged.bind(this)
+    this.DescriptionChanged = this.DescriptionChanged.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleAbort = this.handleAbort.bind(this)
+  }
+
+  TitleChanged (ev) { this.setState({ Title: ev.target.value }) }
+  StartTimeChanged (ev) { this.setState({ StartTime: ev.target.value }) }
+  EndTimeChanged (ev) { this.setState({ EndTime: ev.target.value }) }
+  DescriptionChanged (ev) { this.setState({ Description: ev.target.value }) }
+
+  handleSubmit (ev) {
+    this.props.SubmitActivity(this.state)
+    ev.preventDefault()
+  }
+
+  handleAbort (ev) {
+    this.props.Abort()
+    ev.preventDefault()
+  }
 
   render () {
     return <div>
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h3>Activity Info</h3>
 
         <div className='group'>
-          <label for='name'>Activity Name:</label>
+          <label for='Title'>Title:</label>
           <br />
-          <input id='name' name='Name' type='text' />
+          <input type='text' id='title'
+            value={this.state.Title}
+            onChange={this.TitleChanged} />
         </div>
 
         <div className='group'>
           <label for='start'>Start Time:</label>
           <br />
-          <input id='start' name='StartTime' type='time' />
+          <input type='text' id='start'
+            value={this.state.StartTime}
+            onChange={this.StartTimeChanged} />
         </div>
 
         <div className='group'>
           <label for='end'>End Time:</label>
           <br />
-          <input id='end' name='EndTime' type='time' />
+          <input type='text' id='end'
+            value={this.state.EndTime}
+            onChange={this.EndTimeChanged} />
         </div>
         <br />
 
         <div className='group'>
           <label for='desc'>Description:</label>
           <br />
-          <textarea id='desc' name='Description'
-            cols='50'
-            rows='6'
+          <textarea id='desc' cols='50' rows='6'
+            value={this.state.Description}
+            onChange={this.DescriptionChanged}
           />
         </div>
         <br />
 
-        <input type='submit' value='Save' />
-        <input type='submit' value='Cancel' />
+        <input className='button' type='submit' value='Save' />
+        <button className='button' onClick={this.props.Abort}>Cancel</button>
       </form>
     </div>
   }
--- src/UI/ScheduleView.js
+++ src/UI/ScheduleView.js
@@ -2,17 +2,6 @@ import React from 'react'
 import Schedule from './Schedule'
 import { Redirect } from 'react-router-dom'
 
-// Temp Activities for testing UI
-let activities =
-  [
-    { id: 0, Title: 'Math Time', StartTime: '8:20', EndTime: '10:00' },
-    { id: 1, Title: 'Reading Time', StartTime: '10:00', EndTime: '11:30' },
-    { id: 2, Title: 'Lunch Time', StartTime: '11:00', EndTime: '11:30' },
-    { id: 3, Title: 'Alt Lunch Time', StartTime: '11:00', EndTime: '11:30' },
-    { id: 4, Title: 'Optional PE', StartTime: '11:20', EndTime: '12:00' },
-    { id: 5, Title: 'Math Continued', StartTime: '12:00', EndTime: '13:00' }
-  ]
-
 // Converts Mouse Y coordinate into time marking
 let MouseYToTime = (MouseY) => {
   let TopMargin = 10
@@ -63,7 +52,7 @@ class ScheduleView extends React.Component {
 
     return (
       <div>
-        <Schedule Activities={activities}
+        <Schedule Activities={this.props.Activities}
           TimeIncrement={60}
           ContainerClicked={this.NewActivityEv}
           ItemClicked={this.EditActivityEv} />
